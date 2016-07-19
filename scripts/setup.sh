15baca073f19f4e2a028940fecf24769f2c3c396
Alan Fachini <alfakini@gmail.com>
2016-07-19 18:56:34 -0300

    Change setup order.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,10 +10,10 @@ echo ''
 sh ./scripts/symlink_dotfiles.sh
 
 if [ "$(uname -s)" == "Darwin" ]; then
+  sh ./scripts/set_osx_defaults.sh
   sh ./scripts/install_brew.sh
   sh ./scripts/install_fonts.sh
   sh ./scripts/install_apps.sh
-  sh ./scripts/set_osx_defaults.sh
 fi
 
 echo "Running custom install scripts"
