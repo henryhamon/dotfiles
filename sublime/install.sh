212ac5e66c3eee728490dfe7328946c37fbb45a6
Alan Fachini <alfakini@gmail.com>
2016-07-19 18:56:23 -0300

    Better sublime configs.

--- sublime/install.sh
+++ sublime/install.sh
@@ -3,6 +3,13 @@
 cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd -P)
 
-sublime_origin="$DOTFILES_ROOT/sublime/"
-sublime_dst="$HOME/Library/Application Support/Sublime Text 3"
-ln -sf "$sublime_origin" "$sublime_dst"
+if [ "$(uname -s)" == "Darwin" ]; then
+  sublime_origin="$DOTFILES_ROOT/sublime/"
+  sublime_dst="$HOME/Library/Application Support/Sublime Text 3"
+  ln -sf "$sublime_origin" "$sublime_dst"
+
+  echo "Linking Sublime Text command line"
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+  echo "Setting Git to use Sublime Text as default editor"
+  git config --global core.editor "subl -n -w"
+fi
